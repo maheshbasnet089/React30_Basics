0fc550a94e2e73256cb5f10b631b3dc1aa259aeb
maheshbasnet089 <basnetmanish089@gmail.com>
2024-04-05 20:13:32 +0545

    learn about usestate

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,54 @@
-import Button from "./Button"
+// import { useState } from "react"
+// import Button from "./Button"
 
-function About(){
-    return ( 
-       <Button />
-    )
+// function About(){
+// //     const state = useState("Manish")
+// //    const firstItem = state[0]
+// //     const secondItem = state[1]
+// // var count = 0 
+// // const state = useState(0)
+// // const firstItem = state[0]
+// // const secondItem = state[1]
+//  const [number,setNumber] =  useState(0)
 
+
+// const increaseCount = ()=>{
+//    setNumber(number + 1)
+// }
+//     return ( 
+//        <div>
+//         <h1>{number}</h1>
+//         <button onClick={increaseCount}>+</button>
+//        </div>
+//     )
+
+// }
+
+// export default About
+
+
+import { useState } from "react"
+
+const About = () => {
+
+  const [number, setNumber]= useState({
+    name : "Manish Basnet",
+    address : "Itahari"
+  })
+  const increaseCount =()=>{
+    setNumber(number+1)
+  }
+  const decreaseCount =()=>{
+    setNumber(number-1)
+  }
+  // useState()
+  return (
+    <div>
+        <h2>{number.name}</h2>
+        <button onClick={increaseCount}>+</button>
+        <button onClick={decreaseCount}>-</button>
+    </div>
+  )
 }
 
 export default About
